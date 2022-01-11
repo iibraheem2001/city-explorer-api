1ad4954e181265b4021ee56fd04f579c2901d8fc
Tiara Brown <tiaradominique@gmail.com>
2022-01-10 21:33:43 -0500

    attempting to get server to connect to weatherbitio

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
-const weatherData = require('./data/weather.json');
+const axios = require('axios');
 
 const app = express();
 app.use(cors());
@@ -14,23 +14,29 @@ app.get('/', (request, response) => {
   response.send('Random message!');
 });
 
-app.get('/weather', (req, res) => {
-  const lat = req.query.lat;
-  const lon = req.query.lon;
-  const query = req.query.query || "Seattle";
-  const cityData = weatherData.find(city => city.city_name.toLowerCase() === query.toLowerCase());
-  console.log(cityData);
-  const weatherArr = cityData.data.map(day => new Forcast(day));
-  // console.log("Query Params: ", req.query);
+app.get('/weather', getWeather);
+
+async function getWeather(request, response) {
+  const lat = request.query.lat;
+  const lon = request.query.lon;
+  const url = `http://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lat=${lat}&lon=${lon}`;
+
+  const weatherResponse = await axios.get(url);
+  const weatherArr = weatherResponse.data.results.map(forecast => new Forecast(forecast));
+  // console.log(cityData);
+  // const weatherArr = cityData.data.map(day => new Forcast(day));
+  // console.log("Query Params: ", request.query);
   // console.log("Type: ", type);
-  res.status(200).send(weatherArr);
-});
+  response.status(200).send(weatherArr);
+};
 
 app.get('*', (req, res) => {
   res.status(404).send('page not found');
 });
-class Forcast{
-  constructor(day){
+class Forecast {
+  constructor(day) {
+    this.lat = day.lat
+    this.lon = day.lon
     this.date = day.valid_date;
     this.description = day.weather.description;
   }
